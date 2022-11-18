e9b0aef6bc5ecd71f6546646be17d56e48ea8903
François Voron <fvoron@gmail.com>
2022-11-18 15:15:44 +0100

    Add MongoDB examples

---
+++ chapter6/mongodb/models.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class MongoBaseModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+    class Config:
+        json_encoders = {ObjectId: str}
+
+
+class PostBase(MongoBaseModel):
+    title: str
+    content: str
+    publication_date: datetime = Field(default_factory=datetime.now)
+
+
+class PostPartialUpdate(BaseModel):
+    title: str | None = None
+    content: str | None = None
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    pass
