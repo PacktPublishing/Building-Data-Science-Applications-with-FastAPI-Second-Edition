7dda21c75339a798307e8f1ae99d5bc3772c34a0
François Voron <fvoron@gmail.com>
2023-06-01 15:53:34 +0200

    Improve chapter13_object_detection sample

--- chapter13/chapter13_object_detection.py
+++ chapter13/chapter13_object_detection.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import torch
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from transformers import AutoImageProcessor, AutoModelForObjectDetection
 
 root_directory = Path(__file__).parent.parent
@@ -18,10 +18,12 @@ target_sizes = torch.tensor([image.size[::-1]])
 results = image_processor.post_process(outputs, target_sizes=target_sizes)[0]
 
 draw = ImageDraw.Draw(image)
+font_path = root_directory / "assets" / "OpenSans-ExtraBold.ttf"
+font = ImageFont.truetype(str(font_path), 24)
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     if score > 0.7:
         box_values = box.tolist()
         label = model.config.id2label[label.item()]
-        draw.rectangle(box_values, outline="green")
-        draw.text(box_values[0:2], label, fill="green")
+        draw.rectangle(box_values, outline="red", width=5)
+        draw.text(box_values[0:2], label, fill="red", font=font)
 image.show()
